4fdcc0d113b53be660aba0e035b708b2476b9cc7
irfanhsajid <irfanhsajid@gmail.com>
2021-11-16 14:48:17 +0600

    admin route starts

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -10,7 +10,7 @@ const MakeAdmin = () => {
     }
     const handleAdminSubmit = (e) => {
         const user = { email };
-        fetch('http://localhost:5000/users/admin', {
+        fetch('https://hidden-citadel-95408.herokuapp.com/users/admin', {
             method: 'PUT',
             headers: {
                 'content-type': 'application/json'
--- src/Pages/Login-Register/Hooks/useFirebase.js
+++ src/Pages/Login-Register/Hooks/useFirebase.js
@@ -77,7 +77,7 @@ const useFirebase = () => {
 
 
     useEffect(() => {
-        fetch(`http://localhost:5000/users/${user.email}`)
+        fetch(`https://hidden-citadel-95408.herokuapp.com/users/${user.email}`)
             .then(res => res.json())
             .then(data => setAdmin(data.admin))
     }, [user.email])
@@ -97,7 +97,7 @@ const useFirebase = () => {
 
     const saveUser = (email, displayName, method) => {
         const user = { email, displayName };
-        fetch('http://localhost:5000/users', {
+        fetch('https://hidden-citadel-95408.herokuapp.com/users', {
             method: method,
             headers: {
                 'content-type': 'application/json'
